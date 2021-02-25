84e23a77acbab1e73c2656fdc4020ab829877d26
Joemillard <joeymillard@hotmail.co.uk>
2021-02-25 13:34:53 +0000

    calculate change in exposure for pollination dependent production

--- R/08_pollinator_dependent_climate_projections.R
+++ R/08_pollinator_dependent_climate_projections.R
@@ -1,13 +1,106 @@
 # read in packages
-library(dplyr)
 library(raster)
+library(ggplot2)
+library(rasterVis)
+library(dplyr)
+library(viridis)
 library(rworldmap) 
 library(rworldxtra)
-library(ggplot2)
 library(cowplot)
 
 # source in additional functions
 source("R/00_functions.R")
+
+# list the crop specific folders in the directory for external hard drive
+cropdirs <- list.dirs("G:/Extra_data_files/HarvestedAreaYield175Crops_Geotiff/HarvestedAreaYield175Crops_Geotiff/Geotiff", recursive = FALSE)
+
+# read in the Klein pollinator dependent crops
+klein_cleaned <- read.csv(here::here("data/KleinPollinationDependentCrops.tar/KleinPollinationDependentCrops/data_cleaned.csv"))
+
+# select those with semi colon into a multiple rows
+semi_colon_crop <- klein_cleaned$MonfredaCrop[grepl(";", klein_cleaned$MonfredaCrop)]
+
+# subset those with semi colon and bind as duplicates onto the bottom of dataframe
+semi_colon_crop <- klein_cleaned %>% filter(MonfredaCrop %in% semi_colon_crop) %>%
+  mutate(MonfredaCrop = gsub("lemonlime; ", "", MonfredaCrop)) %>%
+  mutate(MonfredaCrop = gsub("rasberry; ", "", MonfredaCrop)) %>%
+  mutate(MonfredaCrop = gsub("cashew; ", "", MonfredaCrop))
+
+# bind the rows for semi colon crops back onto the original klein data
+klein_cleaned <- rbind(klein_cleaned, semi_colon_crop) %>% 
+  mutate(MonfredaCrop = gsub("; citrusnes", "", MonfredaCrop)) %>%
+  mutate(MonfredaCrop = gsub("; berrynes", "", MonfredaCrop)) %>%
+  mutate(MonfredaCrop = gsub("; cashewapple", "", MonfredaCrop))
+
+# loop through each directory and create a list of all files
+all.ras <- NULL
+crop.files <- list()
+
+for(i in 1:length(cropdirs)){
+  crop.files[[i]] <- list.files(cropdirs[i], pattern = "Production.tif$")
+  crop.files[[i]] <- paste0(cropdirs[i], "/", crop.files[[i]])
+}
+
+# file paths for each of per hectare application
+unlisted_crops <- unlist(crop.files)
+rate_rasters <- list()
+
+# subset the file paths for just those that are pollination dependent to some extent
+# subset as strings to filter from klein_cleaned
+pollinated_crops <- grep(paste(unique(paste("/", klein_cleaned$MonfredaCrop, "_", sep = "")), collapse = "|"), unlisted_crops, value = TRUE)
+pollinat_crops_simp <- gsub("G:/Extra_data_files/HarvestedAreaYield175Crops_Geotiff/HarvestedAreaYield175Crops_Geotiff/Geotiff/", "", pollinated_crops)
+pollinat_crops_simp <- gsub('([^/]+$)', "", pollinat_crops_simp)
+pollinat_crops_simp <- gsub('/', "", pollinat_crops_simp)
+
+# read in each of the rasters
+for(i in 1:length(pollinated_crops)){
+  rate_rasters[[i]] <- raster(pollinated_crops[i])
+  print(i)
+}
+
+# multiple each raster by the percentage attributable to pollination
+# pollination dependent ratios
+# each crop in the monfreda data can have a different pollination dependence
+# 0 = no increase
+# 0.05 = little
+# 0.25 = modest
+# 0.45 = modest/great
+# 0.65 = great
+# 0.95 = essential
+
+# dependence ratio assignment
+klein_cleaned$dependence_ratio[klein_cleaned$Positive.impact.by.animal.pollination == "no increase"] <- 0
+klein_cleaned$dependence_ratio[klein_cleaned$Positive.impact.by.animal.pollination == "little"] <- 0.05
+klein_cleaned$dependence_ratio[klein_cleaned$Positive.impact.by.animal.pollination == "modest"] <- 0.25
+klein_cleaned$dependence_ratio[klein_cleaned$Positive.impact.by.animal.pollination == "great"] <- 0.65
+klein_cleaned$dependence_ratio[klein_cleaned$Positive.impact.by.animal.pollination == "essential"] <- 0.95
+klein_cleaned$dependence_ratio[klein_cleaned$Positive.impact.by.animal.pollination == "modest/great"] <- 0.45
+
+# calculate average and standard deviation of pollination dependence for each Monfreda crop
+klein_cleaned_av <- klein_cleaned %>%
+  group_by(MonfredaCrop) %>%
+  mutate(av = mean(dependence_ratio, na.rm = TRUE)) %>%
+  mutate(standard_dev = sd(dependence_ratio, na.rm = TRUE)) %>%
+  ungroup() %>%
+  select(MonfredaCrop, av, standard_dev) %>%
+  unique()
+
+# subset klein_cleaned for those with crop data
+klein_cleaned_filt <- klein_cleaned_av %>%
+  filter(MonfredaCrop %in% pollinat_crops_simp) %>%
+  arrange(MonfredaCrop)
+
+# multiply each raster by its pollination dependence for that crop
+rate_rasters_adj <- list()
+for(i in 1:length(rate_rasters)){
+  rate_rasters_adj[[i]] <- rate_rasters[[i]] * klein_cleaned_filt$av[i]
+  print(i)
+}
+
+# sum the production for all the rasters
+# organise all of the rasters into a stack and sum
+stacked_rasters <- stack(rate_rasters_adj)
+crop.total <- sum(stacked_rasters, na.rm = T)
 
 # read in the raster for the historical data to get the baseline
 ## standardised climate anomaly script
@@ -104,4 +197,49 @@ for(i in 1:length(years_list)){
   tmp2069_71std_climate_anomaly[[i]] <- (mean.temp.2069.2071-tmp1901_1931mean)  / tmp1901_1931sd
 
 }
+
+std_anom_high <- list()
+
+# for each set of climate anomaly data, subset for greater than equal to 4
+# select all pollination dependence production at greater than 4 anomaly and sum
+for(i in 1:length(tmp2069_71std_climate_anomaly)){
+  
+  # convert the raster to a dataframe to plot with ggplot
+  std_anom_high[[i]] <- as(tmp2069_71std_climate_anomaly[[i]], "SpatialPixelsDataFrame")
+  std_anom_high[[i]] <- as.data.frame(std_anom_high[[i]])
+  
+  # assign 0 in layer as NA
+  std_anom_high[[i]]$layer[std_anom_high[[i]]$layer == 0] <- NA
+  
+  # convert spatial dataframe to coordinates
+  std_anom_high[[i]] <- std_anom_high[[i]] %>%
+    dplyr::filter(layer >= 4) %>%
+    dplyr::select(x, y) %>%
+    unique() %>%
+    SpatialPoints()
+  
+  print(length(std_anom_high[[i]]))
+  
+}
+
+# set up vector for total production
+vulnerable_production_list <- list()
+vulnerable_production <- c()
+
+# for each set of coordinates, extract the pollination dependent values and sum
+for(i in 1:length(std_anom_high)){
+
+  # convert the climate anomaly raster to a spatial pixels data frame, and then rename the columns
+  vulnerable_production_list[[i]] <- extract(crop.total, std_anom_high[[i]], na.rm = FALSE)
+  vulnerable_production[i] <- unlist(vulnerable_production_list[[i]]) %>% sum()
+}
+
+# create dataframe for exposed production and build datafrmae
+data.frame("production" = vulnerable_production, "year" = c(seq(2046, 2016, -3))) %>%
+  ggplot() +
+    geom_line(aes(x = year, y = production)) +
+    geom_point(aes(x = year, y = production)) +
+    scale_y_continuous(limits = c(0, 210000), expand = c(0, 0)) +
+    theme_bw() +
+    theme(panel.grid = element_blank())
 
