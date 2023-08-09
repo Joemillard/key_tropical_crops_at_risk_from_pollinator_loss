1febe5b96d6f48347834d3bc516dc606302a5205
Joe Millard <joseph.millard.17@ucl.ac.uk>
2023-08-09 16:57:06 +0100

    remove extra code
    remove extra code not going in paper

--- R/02g_models_insect-pollinator_insect-non-pollinator_mean_annual_temp.R
+++ R/02g_models_insect-pollinator_insect-non-pollinator_mean_annual_temp.R
@@ -243,51 +243,6 @@ predicts_climate %>%
 
 ggsave("correlation_mean_temp_anomaly.png", scale = 1, dpi = 350)
 
-# resample sites to get distribution of anomaly and mean temp within lowest random effect nesting
-SS_vec <- unique(predicts_climate$SS)
-land_use_vec <- unique(predicts_climate$Predominant_land_use)
-
-climate_samples <- list()
-land_use_samples <- list()
-
-# iterate through each of the land use classes
-for(j in 1:length(land_use_vec)){
-  # for each of the SSB, calculate the mean for cropland and primary for mean temp and anomaly
-  for(i in 1:length(SS_vec)){
-    climate_samples[[i]] <- predicts_climate %>%
-      filter(Predominant_land_use == land_use_vec[j]) %>%
-      filter(SS %in% SS_vec[i]) %>%
-      droplevels() %>%
-      group_by(Predominant_land_use) %>%
-      summarise(mean_sample = sd(mean_value, na.rm = TRUE))
-    
-  }
-  
-  land_use_samples[[j]] <- climate_samples %>%
-    rbindlist()
-  
-}
-
-rbindlist(land_use_samples) %>%
-  ggplot() +
-    geom_boxplot(aes(y = log(mean_sample), x = Predominant_land_use, fill = Predominant_land_use, colour = Predominant_land_use), alpha = 0.2) +
-      scale_fill_manual("Land-use type", values = c("#009E73", "#E69F00")) +
-      scale_colour_manual("Land-use type", values = c("#009E73", "#E69F00")) +
-      scale_y_continuous("Std. dev. of SS mean annual temp.", breaks = c(-3.465736, -2.7725887, -2.0794415, -1.3862944, -0.6931472, 0), 
-                         labels = c(0.03125, 0.0625, 0.125, 0.25, 0.5, 1)) +
-      xlab("") +
-      theme_bw() +
-      theme(panel.grid = element_blank())
-
-# save plot for mean annual temp variation
-ggsave("Mean_temp_std_dev.png", scale = 1, dpi = 350)
-
-# bring in basemap for climate site plot 
-base_map <- get_basemap()
-
-# fortify the main map
-map_fort <- fortify(base_map)
-
 # plot the climate anom for each taxonomic order
 predicts_climate %>%
   ggplot() +
@@ -386,23 +341,19 @@ model_abundance_anom <- lmerTest::lmer(log(Total_abundance) ~ standard_anom * Pr
 model_abundance_anom_mean <- lmerTest::lmer(log(Total_abundance) ~ mean_value + standard_anom * Predominant_land_use + (1|SS) + (1|SSB), data = predicts_climate) 
 model_abundance_anom_all <- lmerTest::lmer(log(Total_abundance) ~ mean_value + mean_value:Predominant_land_use + standard_anom * Predominant_land_use + (1|SS) + (1|SSB), data = predicts_climate) 
 
+summary(model_abundance_anom_all)
+summary(model_abundance_anom)
+
+
 ### filter for just primary or cropland
 predicts_climate_primary <- predicts_climate %>%
   filter(Predominant_land_use == "Primary vegetation")
 
 # species richness, standard anom as a factor
 pri_model_abundance_mean_anom <- lmerTest::lmer(log(Total_abundance) ~ mean_value + standard_anom + (1|SS) + (1|SSB), data = predicts_climate_primary)
-pri_model_abundance_mean <- lmerTest::lmer(log(Total_abundance) ~ mean_value  + (1|SS) + (1|SSB), data = predicts_climate_primary) 
-pri_model_abundance_anom <- lmerTest::lmer(log(Total_abundance) ~ standard_anom + (1|SS) + (1|SSB), data = predicts_climate_primary) 
-pri_model_abundance_anom_non_stan <- lmerTest::lmer(log(Total_abundance) ~ anomaly + (1|SS) + (1|SSB), data = predicts_climate_primary) 
 
 # extract model summaries
 summary(pri_model_abundance_mean_anom)
-summary(pri_model_abundance_mean)
-summary(pri_model_abundance_anom)
-summary(pri_model_abundance_anom_non_stan)
-
-AIC(pri_model_abundance_mean_anom, pri_model_abundance_mean, pri_model_abundance_anom, pri_model_abundance_anom_non_stan)
 
 # filter for just primary
 predicts_climate_cropland <- predicts_climate %>%
@@ -410,127 +361,6 @@ predicts_climate_cropland <- predicts_climate %>%
 
 # species richness, standard anom as a factor
 cro_model_abundance_mean_anom <- lmerTest::lmer(log(Total_abundance) ~ mean_value + standard_anom + (1|SS) + (1|SSB), data = predicts_climate_cropland)
-cro_model_abundance_mean <- lmerTest::lmer(log(Total_abundance) ~ mean_value  + (1|SS) + (1|SSB), data = predicts_climate_cropland) 
-cro_model_abundance_anom <- lmerTest::lmer(log(Total_abundance) ~ standard_anom + (1|SS) + (1|SSB), data = predicts_climate_cropland) 
-cro_model_abundance_anom_non_stan <- lmerTest::lmer(log(Total_abundance) ~ anomaly + (1|SS) + (1|SSB), data = predicts_climate_cropland) 
 
 # extract model summaries
 summary(cro_model_abundance_mean_anom)
-summary(cro_model_abundance_mean)
-summary(cro_model_abundance_anom)
-summary(cro_model_abundance_anom_non_stan)
-
-AIC(cro_model_abundance_mean_anom, cro_model_abundance_mean, cro_model_abundance_anom, cro_model_abundance_anom_non_stan)
-
-# run predictions for the model of standard anomaly
-abundance_model <- predict_continuous(model = model_abundance_mean_val,
-                                           model_data = predicts_climate,
-                                           response_variable = "Total_abundance",
-                                           categorical_variable = c("Predominant_land_use"),
-                                           continuous_variable = c("mean_value"),
-                                           continuous_transformation = "",
-                                           random_variable = c("SS", "SSB", "SSBS"))
-
-# plot for standardised anomaly and land-use for abundance
-main_plot_abundance <- ggplot(abundance_model) +
-  geom_line(aes(x = mean_value, y = y_value, colour = Predominant_land_use), size = 1.5) +
-  geom_ribbon(aes(x = mean_value, y = y_value, fill = Predominant_land_use, ymin = y_value_minus, ymax = y_value_plus), alpha = 0.4) +
-  scale_fill_manual("Land-use type", values = c("#009E73", "#E69F00")) +
-  scale_y_continuous(limits = c(0.3, 6.5), breaks = c(1.609438, 2.302585, 2.995732, 3.6888795, 4.382027, 5.075174, 5.768321, 6.461468), labels = c(5, 10, 20, 40, 80, 160, 320, 640)) +
-  scale_colour_manual("Land-use type", values = c("#009E73", "#E69F00")) +
-  xlab("Mean temperature") +
-  ylab("Total abundance") +
-  theme_bw() +
-  theme(panel.grid = element_blank())
-
-# code that randomsly samples sliding numbers from cropland and primary vegetation, and then checks how delta AIC changes
-# hypothesis being that as the ratio moves towards primary vegetation, mean temperature better, explains, and then towards cropland standardised anomaly better explains
-predicts_climate %>%
-  group_by(Predominant_land_use) %>%
-  tally()
-
-# set up list for random filter values
-filter_list <- list(c(100, 1100), 
-     c(200, 1000), 
-     c(300, 900), 
-     c(400, 800), 
-     c(500, 700),
-     c(600, 600), 
-     c(700, 500),
-     c(800, 400),
-     c(900, 300),
-     c(1000, 200),
-     c(1100, 100))
-
-delta_AIC_list <- list()
-
-# iterate through each iteration for the number randomly sampled
-for(i in 1:length(filter_list)){
-  
-  # vector for delta AIC values
-  delta_AIC <- c()
-  
-  for(j in 1:200){
-    
-    # filter for cropland and randomly sample rows for that simulation
-    predicts_climate_cropland <- predicts_climate %>%
-      filter(Predominant_land_use == "Cropland") %>%
-      sample_n(filter_list[[i]][1]) %>%
-      droplevels()
-    
-    # filter for primary vegetation and randomly sample rows for that simulation
-    predicts_climate_prim <- predicts_climate %>%
-      filter(Predominant_land_use == "Primary vegetation") %>%
-      sample_n(filter_list[[i]][2]) %>%
-      droplevels()
-    
-    # bind together the random samples for cropland and primary vegetation
-    predicts_climate_sampled <- rbind(predicts_climate_cropland, predicts_climate_prim)
-    
-    # build models for both mean value and standardised temperature anomaly
-    model_abundance_mean_val<- lmerTest::lmer(log(Total_abundance) ~ Predominant_land_use * mean_value + (1|SS) + (1|SSB), data = predicts_climate_sampled)
-    model_abundance_stan_anom <- lmerTest::lmer(log(Total_abundance) ~ Predominant_land_use * standard_anom + (1|SS) + (1|SSB), data = predicts_climate_sampled)
-    
-    # calculate delta AIC per simulation
-    delta_AIC[j] <- AIC(model_abundance_mean_val) - AIC(model_abundance_stan_anom)
-    
-  }
-  
-  delta_AIC_list[[i]] <- data.frame("delta_AIC" = delta_AIC, 
-                                    "Cropland_sites" = filter_list[[i]][[1]], 
-                                    "Primary_sites" = filter_list[[i]][[2]])
-  
-}
-
-# bind together the simulations and calculate median delta AIC
-rbindlist(delta_AIC_list) %>%
-  group_by(Cropland_sites, Primary_sites) %>%
-  summarise(lower = quantile(delta_AIC, 0.025), median_delta_AIC = median(delta_AIC), upper = quantile(delta_AIC, 0.975)) %>%
-  ggplot() +
-    geom_point(aes(x = Cropland_sites/(Cropland_sites+Primary_sites), y = median_delta_AIC)) +
-    geom_errorbar(aes(x = Cropland_sites/(Cropland_sites+Primary_sites), ymin = lower, ymax = upper)) +
-    geom_hline(yintercept = 0, alpha = 0.2) + 
-    scale_x_continuous("Cropland site proportion", breaks = c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1)) +
-    ylab("Delta AIC (Mean - Std. anomaly)") +
-    theme_bw() +
-    theme(panel.grid = element_blank())
- 
-ggsave("resampled_delta_AIC.png", scale = 1, dpi = 350)
-
-# test for effect within one land use class and intensiy
-predicts_cropland_light_use <- predicts_climate %>%
-  filter(Predominant_land_use %in% "Cropland") %>%
-  filter(Use_intensity == "Light use")
-
-model_abundance_mean_val_light <- lmerTest::lmer(log(Total_abundance) ~ mean_value + (1|SS) + (1|SSB), data = predicts_cropland_light_use)
-model_abundance_stan_anom_light <- lmerTest::lmer(log(Total_abundance) ~ standard_anom + (1|SS) + (1|SSB), data = predicts_cropland_light_use)
-
-AIC(model_abundance_mean_val_light, model_abundance_stan_anom_light)
-
-# test for effect in one sampling method
-predicts_climate <- predicts_climate %>%
-  filter()
-
-model_abundance_mean_val_light <- lmerTest::lmer(log(Total_abundance) ~ mean_value + (1|SS) + (1|SSB), data = predicts_climate)
-model_abundance_stan_anom_light <- lmerTest::lmer(log(Total_abundance) ~ standard_anom + (1|SS) + (1|SSB), data = predicts_climate)
-
